bdbd2f15a3cdc993cf6e5d43082c0c5989146a79
Asouvane <alexia.souvane@gmail.com>
2019-05-15 12:37:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      liste:[]
+    }
+  }
+
+  ajouterElement = () => {
+
+    if (this.state.input !== "") {
+      this.setState({
+        liste: [...this.state.liste, this.state.input],
+        input: ""
+      });
+    }
+
+  };
+
+  render () {
+
+    const elements = this.state.liste.map((elem,index) => 
+      <div key={index} className='element'>{elem} <button onClick={() => { 
+        this.setState( {
+          liste: this.state.liste.filter((e, indexToRemove) => index !== indexToRemove )
+        });
+      }}>X</button></div>
+    )
+
+    return (
+      <div className="App">
+        <h2>ToDoList</h2>
+        <div>
+          <input
+            id='input'
+            value={this.state.input}
+            onChange= {(input) => this.setState ({ 
+              input: input.target.value
+            })}
+          />
+          <div id="valider" onClick={() => this.ajouterElement()}>Valider</div>
+        </div>
+
+        <div id='elements'>
+          {elements}
+        </div>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { mount } from "enzyme";
+import Enzyme from "enzyme";
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+
+let expectInputVide = (input) => {
+	expect(input.html()).toContain('value=""');
+}
+
+let expectNoElements = (app) => {
+   	expect(app.find('.element').exists()).toBe(false);
+}
+
+let ecrire = (texte) => {
+
+	return {
+		dans(input) {
+			input.simulate("change", {
+  				target: {
+  					value: texte
+  				}
+  			});
+		}
+	}
+}
+
+let cliquerSur = (bouton) => {
+  	bouton.simulate("click");
+}
+
+
+
+let ajouterElement = (texte, app) => {
+	ecrire(texte).dans(app.find("#input"));
+	cliquerSur(app.find('#valider'));
+} 
+
+
+describe('App', () => {
+  it('affiche une todo vide', () => {
+  	const app = mountApp();
+
+    expect(app.html()).toContain("<h2>ToDoList</h2>");
+    // expect(app.state().liste).toHaveLength(0);
+    expectInputVide(app.find('#input'));
+	expectNoElements(app);	  
+  });
+
+  it('ajouter un élément', () => {
+  	const app = mountApp();
+
+  	ajouterElement("Faire les courses", app)
+  	expect(app.find('#elements').html()).toContain("Faire les courses");
+  	expectInputVide(app.find('#input'));
+
+  });
+
+  it('retirer un élément', () => {
+  	const app = mountApp();
+
+  	ajouterElement("Faire les courses", app);
+  	app.find("#elements").first().find('button').simulate('click');
+  	expectNoElements(app);
+
+  });
+
+
+  it("ne pas ajouter input vide", () => {
+  	const app = mountApp();
+
+  	ajouterElement("", app);
+  	expectNoElements(app);
+  });
+
+
 });
+
+
+let mountApp = function() {
+  return mount(<App />);
+};
